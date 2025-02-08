e0050c9ca1522ab9e4d14409fee7dce3457e66b6
CristianoSFMothe <cristianodevsystem@gmail.com>
2025-02-08 00:50:15 -0300

    feat: create middleware authenticated

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,6 +1,8 @@
 import { Router, Request, Response } from "express";
 import { CreateUserController } from "./controllers/user/CreateUserController";
 import { AuthUserController } from "./controllers/user/AuthUserController";
+import { DetailsUserController } from "./controllers/user/DetailsUserController";
+import { isAuthenticated } from "./middlewares/isAuthenticated";
 
 const router = Router();
 
@@ -10,5 +12,6 @@ router.post("/users", new CreateUserController().handle);
 
 // --- ROTAS AUTH ---
 router.post("/session", new AuthUserController().handler);
+router.get("/me", isAuthenticated, new DetailsUserController().handle);
 
 export { router };
